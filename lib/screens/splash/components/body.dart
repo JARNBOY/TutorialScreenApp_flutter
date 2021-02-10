9dc0221b3d8a1a5520c8951238cfd6c40772fa61
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-10 21:53:14 +0700

    add splash mock data , lenght

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -8,6 +8,21 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to E-Comemerce, Let’s shop!",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text": "We help people conect with store \naround Thailand of Thai",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text": "We show the easy way to shop. \nJust stay at home with th",
+      "image": "assets/images/splash_3.png"
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -18,6 +33,7 @@ class _BodyState extends State<Body> {
             Expanded(
               flex: 3,
               child: PageView.builder(
+                itemCount: splashData.length,
                 itemBuilder: (context, index) => SplashContent(
                   image: "assets/images/splash_1.png",
                   text: "Welcome to E-Comemerce,Let's shop!",
