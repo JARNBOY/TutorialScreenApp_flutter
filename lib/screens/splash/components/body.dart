7eb1b45340da071dbe89bd4f14a3bbec5067130e
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-10 21:13:32 +0700

    add title , set column splash content

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tutorialscreenapp_flutter/constant.dart';
+import 'package:tutorialscreenapp_flutter/size_config.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -8,6 +10,51 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: SplashContent(),
+            ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Spacer(),
+        Text(
+          "E-Comemerce",
+          style: TextStyle(
+              fontSize: getProportionateScreenWidth(36),
+              color: kPrimaryColor,
+              fontWeight: FontWeight.bold),
+        ),
+        Text("Welcome to E-Comemerce,Let's shop!"),
+        Spacer(),
+        Image.asset(
+          "assets/images/splash_1.png",
+          height: getProportionateScreenHeight(265),
+          width: getProportionateScreenWidth(235),
+        )
+      ],
+    );
   }
 }
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:tutorialscreenapp_flutter/screens/splash/components/body.dart';
+import 'package:tutorialscreenapp_flutter/size_config.dart';
 
 class SplassScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //You have to call ot own your start screen
+    SizeConfig().init(context);
     return Scaffold(
-      body: Container(),
+      body: Body(),
     );
   }
 }
