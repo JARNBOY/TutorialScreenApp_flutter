6dbcb6d3dd56eb80a70fa14b93d80ad2fd8bc1a2
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-11 20:05:59 +0700

    create SocialCard widget

--- lib/screens/splash/sign_in/components/body_signin.dart
+++ lib/screens/splash/sign_in/components/body_signin.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tutorialscreenapp_flutter/components/custom_suffix_icons.dart';
-import 'package:tutorialscreenapp_flutter/components/default_button.dart';
-import 'package:tutorialscreenapp_flutter/components/form_error.dart';
-import 'package:tutorialscreenapp_flutter/constant.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tutorialscreenapp_flutter/screens/splash/sign_in/components/sign_form.dart';
 import 'package:tutorialscreenapp_flutter/size_config.dart';
 
 class BodySignIn extends StatelessWidget {
@@ -27,7 +25,10 @@ class BodySignIn extends StatelessWidget {
               "Sign in eith your email and password \nor continue with social media",
               textAlign: TextAlign.center,
             ),
-            SignForm()
+            SignForm(),
+            SocialCard(
+              svgSocial: "assets/icons/facebook-2.svg",
+            )
           ],
         ),
       ),
@@ -35,128 +36,23 @@ class BodySignIn extends StatelessWidget {
   }
 }
 
-class SignForm extends StatefulWidget {
-  @override
-  _SignFormState createState() => _SignFormState();
-}
-
-class _SignFormState extends State<SignForm> {
-  final _formKey = GlobalKey<FormState>();
-  final List<String> errors = [];
-  String email;
-  String password;
+class SocialCard extends StatelessWidget {
+  const SocialCard({
+    Key key,
+    this.svgSocial,
+    this.press,
+  }) : super(key: key);
+  final String svgSocial;
+  final Function press;
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          emailTextFormField(),
-          SizedBox(
-            height: 20,
-          ),
-          passwordTextFormField(),
-          SizedBox(
-            height: 20,
-          ),
-          FormErrors(errors: errors),
-          SizedBox(
-            height: 20,
-          ),
-          DefaultButton(
-              text: "Continue",
-              press: () {
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                }
-              }),
-        ],
-      ),
-    );
-  }
-
-  TextFormField emailTextFormField() {
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (newValue) => email = newValue,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.remove(kEmailNullError);
-          });
-        }
-
-        if (emailValidatorRegExp.hasMatch(value) &&
-            errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.remove(kInvalidEmailError);
-          });
-        }
-      },
-      validator: (value) {
-        if (value.isEmpty && !errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.add(kEmailNullError);
-          });
-        }
-
-        if (!emailValidatorRegExp.hasMatch(value) &&
-            !errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.add(kInvalidEmailError);
-          });
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-          labelText: "Email",
-          hintText: "Enter your Email",
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          suffixIcon: CustomSuffixIcons(
-            svgIcon: "assets/icons/Mail.svg",
-          )),
-    );
-  }
-
-  TextFormField passwordTextFormField() {
-    return TextFormField(
-      obscureText: true,
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (newValue) => password = newValue,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kPassNullError)) {
-          setState(() {
-            errors.remove(kPassNullError);
-          });
-        }
-
-        if (value.length >= 8 && errors.contains(kShortPassError)) {
-          setState(() {
-            errors.remove(kShortPassError);
-          });
-        }
-      },
-      validator: (value) {
-        if (value.isEmpty && !errors.contains(kPassNullError)) {
-          setState(() {
-            errors.add(kPassNullError);
-          });
-        }
-
-        if (value.length < 8 && !errors.contains(kShortPassError)) {
-          setState(() {
-            errors.add(kShortPassError);
-          });
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-          labelText: "Password",
-          hintText: "Enter your Password",
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          suffixIcon: CustomSuffixIcons(
-            svgIcon: "assets/icons/Lock.svg",
-          )),
+    return Container(
+      padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+      height: getProportionateScreenHeight(40),
+      width: getProportionateScreenWidth(40),
+      decoration:
+          BoxDecoration(color: Color(0xFFF5F6F9), shape: BoxShape.circle),
+      child: SvgPicture.asset(svgSocial),
     );
   }
 }
