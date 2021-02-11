513f1c75aa77eae794a24eb1acef15d7eaa15cc4
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-11 20:24:28 +0700

    add widget SingleChildScrollView for scroll when edit

--- lib/screens/splash/sign_in/components/body_signin.dart
+++ lib/screens/splash/sign_in/components/body_signin.dart
@@ -13,63 +13,65 @@ class BodySignIn extends StatelessWidget {
       child: Padding(
         padding:
             EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-        child: Column(
-          children: [
-            SizedBox(height: SizeConfig.screenHeight * 0.04),
-            Text(
-              "Welcome Back",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: getProportionateScreenWidth(28),
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Sign in eith your email and password \nor continue with social media",
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: SizeConfig.screenHeight * 0.08),
-            SignForm(),
-            SizedBox(height: SizeConfig.screenHeight * 0.08),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SocialCard(
-                  svgSocial: "assets/icons/facebook-2.svg",
-                  press: () {
-                    print("facebook click");
-                  },
-                ),
-                SocialCard(
-                  svgSocial: "assets/icons/twitter.svg",
-                  press: () {
-                    print("twitter click");
-                  },
-                ),
-                SocialCard(
-                  svgSocial: "assets/icons/google-icon.svg",
-                  press: () {
-                    print("google click");
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: getProportionateScreenHeight(20)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account? ",
-                  style: TextStyle(fontSize: getProportionateScreenWidth(16)),
-                ),
-                Text(
-                  "Sign Up",
-                  style: TextStyle(
-                      fontSize: getProportionateScreenWidth(16),
-                      color: kPrimaryColor),
-                )
-              ],
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.04),
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Sign in eith your email and password \nor continue with social media",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.08),
+              SignForm(),
+              SizedBox(height: SizeConfig.screenHeight * 0.08),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialCard(
+                    svgSocial: "assets/icons/facebook-2.svg",
+                    press: () {
+                      print("facebook click");
+                    },
+                  ),
+                  SocialCard(
+                    svgSocial: "assets/icons/twitter.svg",
+                    press: () {
+                      print("twitter click");
+                    },
+                  ),
+                  SocialCard(
+                    svgSocial: "assets/icons/google-icon.svg",
+                    press: () {
+                      print("google click");
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: getProportionateScreenHeight(20)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
+                    style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+                  ),
+                  Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                        color: kPrimaryColor),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ));
