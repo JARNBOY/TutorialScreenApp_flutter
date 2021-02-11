7662ebfc8f247e5c2ede1d0365019090996cfd73
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-11 21:25:31 +0700

    SET APP BAR OF LOGIN SUCCESS

--- lib/screens/splash/login_success/login_success.dart
+++ lib/screens/splash/login_success/login_success.dart
@@ -6,6 +6,8 @@ class LoginSuccessScreen extends StatelessWidget {
   static String routeName = "/login_success";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: BodyLoginSuccess());
+    return Scaffold(
+        appBar: AppBar(leading: SizedBox(), title: Text("Login Success")),
+        body: BodyLoginSuccess());
   }
 }
