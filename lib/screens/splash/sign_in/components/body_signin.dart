a5b84f4d01ae88fe56c3b2e3f90fc467c4e8822c
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-11 18:26:44 +0700

    log label text potion in borfer of textFormField

--- lib/screens/splash/sign_in/components/body_signin.dart
+++ lib/screens/splash/sign_in/components/body_signin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tutorialscreenapp_flutter/constant.dart';
 import 'package:tutorialscreenapp_flutter/size_config.dart';
 
 class BodySignIn extends StatelessWidget {
@@ -7,21 +9,60 @@ class BodySignIn extends StatelessWidget {
     return SafeArea(
         child: SizedBox(
       width: double.infinity,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: Column(
+          children: [
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Sign in eith your email and password \nor continue with social media",
+              textAlign: TextAlign.center,
+            ),
+            SignForm()
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class SignForm extends StatefulWidget {
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
       child: Column(
         children: [
-          Text(
-            "Welcome Back",
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: getProportionateScreenWidth(28),
-                fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "Sign in eith your email and password \nor continue with social media",
-            textAlign: TextAlign.center,
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: "Email",
+                hintText: "Enter your Email",
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                suffixIcon: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0,
+                      getProportionateScreenWidth(20),
+                      getProportionateScreenWidth(20),
+                      getProportionateScreenWidth(20)),
+                  child: SvgPicture.asset(
+                    "assets/icons/Mail.svg",
+                    height: getProportionateScreenWidth(20),
+                  ),
+                )),
           )
         ],
       ),
-    ));
+    );
   }
 }
