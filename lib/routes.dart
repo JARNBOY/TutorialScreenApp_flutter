178dfb3c77ea62419b999c2333af3bd69772bb33
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-11 16:52:51 +0700

    add action press in skip

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,12 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:tutorialscreenapp_flutter/screens/splash/sign_in/sign_in_screen.dart';
 import 'package:tutorialscreenapp_flutter/screens/splash/splash_screen.dart';
 
 //We use  name route
 //All our route will be avialable here
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
 };
