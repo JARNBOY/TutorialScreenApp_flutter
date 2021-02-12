3e3e8ef4fc68285db9aa73621f531316a05aaa27
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-12 12:36:24 +0700

    add  constant headingStyle

---
+++ lib/screens/splash/sign_up/components/body_sign_up.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tutorialscreenapp_flutter/constant.dart';
+
+class BodySignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Register Account",
+          style: headingStyle,
+        )
+      ],
+    );
+  }
+}
--- lib/screens/splash/sign_up/sign_up_screen.dart
+++ lib/screens/splash/sign_up/sign_up_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tutorialscreenapp_flutter/screens/splash/sign_up/components/body_sign_up.dart';
 
 class SignUpScreen extends StatelessWidget {
   static String routeName = "/sign_up";
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign Up"),
+      ),
+      body: BodySignUpScreen(),
+    );
   }
 }
