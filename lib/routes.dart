0f0d921c04430b980eef40ea2ea02f93f217f795
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-12 12:20:47 +0700

    signUpScreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tutorialscreenapp_flutter/screens/splash/forgot_password/forgot_password_screen.dart';
 import 'package:tutorialscreenapp_flutter/screens/splash/login_success/login_success.dart';
 import 'package:tutorialscreenapp_flutter/screens/splash/sign_in/sign_in_screen.dart';
+import 'package:tutorialscreenapp_flutter/screens/splash/sign_up/sign_up_screen.dart';
 import 'package:tutorialscreenapp_flutter/screens/splash/splash_screen.dart';
 
 //We use  name route
@@ -11,4 +12,5 @@ final Map<String, WidgetBuilder> routes = {
   SignInScreen.routeName: (context) => SignInScreen(),
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen()
 };
