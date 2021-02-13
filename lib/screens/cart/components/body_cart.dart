025ed0f779388d6a7f50ab241e766fa84bfc4349
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-13 16:58:34 +0700

    ListView

--- lib/screens/cart/components/body_cart.dart
+++ lib/screens/cart/components/body_cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:tutorialscreenapp_flutter/constant.dart';
 import 'package:tutorialscreenapp_flutter/models/Cart.dart';
 import 'package:tutorialscreenapp_flutter/size_config.dart';
@@ -8,12 +9,22 @@ import 'cart_item_card.dart';
 class BodyCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CartItemCard(
+    return ListView.builder(
+      itemBuilder: (context, index) => Dismissible(
+        key: Key(demoCarts[0].product.id.toString()),
+        background: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: Color(0xFFFFE6E6),
+              borderRadius: BorderRadius.circular(15)),
+          child: Row(
+            children: [Spacer(), SvgPicture.asset("assets/icons/Trash.svg")],
+          ),
+        ),
+        child: CartItemCard(
           cart: demoCarts[0],
         ),
-      ],
+      ),
     );
   }
 }
