b0173d1f3bdf18d48374f4d41e8f9ca4f9d911b7
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-13 17:04:35 +0700

    set DismissDirection.endToStart

--- lib/screens/cart/components/body_cart.dart
+++ lib/screens/cart/components/body_cart.dart
@@ -9,20 +9,32 @@ import 'cart_item_card.dart';
 class BodyCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) => Dismissible(
-        key: Key(demoCarts[0].product.id.toString()),
-        background: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          decoration: BoxDecoration(
-              color: Color(0xFFFFE6E6),
-              borderRadius: BorderRadius.circular(15)),
-          child: Row(
-            children: [Spacer(), SvgPicture.asset("assets/icons/Trash.svg")],
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ListView.builder(
+        itemCount: demoCarts.length,
+        itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Dismissible(
+            key: Key(demoCarts[index].product.id.toString()),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Color(0xFFFFE6E6),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  Spacer(),
+                  SvgPicture.asset("assets/icons/Trash.svg")
+                ],
+              ),
+            ),
+            child: CartItemCard(
+              cart: demoCarts[index],
+            ),
           ),
-        ),
-        child: CartItemCard(
-          cart: demoCarts[0],
         ),
       ),
     );
