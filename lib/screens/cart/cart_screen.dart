f28dc6146ad8d01968359b70c3d7e5a8f1f6e132
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-13 17:39:55 +0700

    BodyCart

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tutorialscreenapp_flutter/components/default_button.dart';
+import 'package:tutorialscreenapp_flutter/constant.dart';
 import 'package:tutorialscreenapp_flutter/models/Cart.dart';
+import 'package:tutorialscreenapp_flutter/size_config.dart';
 
 import 'components/body_cart.dart';
 
@@ -23,6 +27,81 @@ class CartScreen extends StatelessWidget {
         ),
       ),
       body: BodyCart(),
+      bottomNavigationBar: CheckOurCard(),
+    );
+  }
+}
+
+class CheckOurCard extends StatelessWidget {
+  const CheckOurCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          vertical: getProportionateScreenWidth(15),
+          horizontal: getProportionateScreenWidth(30)),
+      // height: 174,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, -15),
+                blurRadius: 20,
+                color: Color(0xFFDADADA).withOpacity(0.15))
+          ]),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  height: getProportionateScreenWidth(40),
+                  width: getProportionateScreenWidth(40),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF5F6F9),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: SvgPicture.asset("assets/icons/receipt.svg"),
+                ),
+                Spacer(),
+                Text("Add voucher code"),
+                //here width is fixed that's why use const
+                const SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 12,
+                  color: kTextColor,
+                )
+              ],
+            ),
+            SizedBox(
+              height: getProportionateScreenWidth(20),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(TextSpan(
+                    text: "Tatol:\n", children: [TextSpan(text: "\$337.15")])),
+                SizedBox(
+                  width: getProportionateScreenWidth(190),
+                  child: DefaultButton(
+                    text: "Purchase",
+                    press: () {},
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
